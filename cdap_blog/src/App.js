e0ad2aa5fc6e527ebd43ba0158e4ba149751dad7
seungji <dkstmdwl0615@naver.com>
2023-02-22 13:49:34 +0900

    part1 : 5번째 강의 진행중

--- cdap_blog/src/App.js
+++ cdap_blog/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '코딩애플로 다시 시작하는 리액트';
+  let [time,b] = useState('현재시간 10:42');
+  // let [logo, setLogo] = useState('ReactBlog')
+  let [title, setTitle] = useState(['JSX','typeScript','react'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+      <h4 style={{color:'antiquewhite', fontSize:'2rem'}}>
+        ReactBlog
+      </h4>
+     </div>
+     <div class="list">
+       <h4>{post}</h4>
+       <p>23.02.22 시작</p>
+       <p>{time}</p>
+     </div>
+     <div class="list">
+       <h4>{title[0]}</h4>
+       <p>23.02.22 시작</p>
+       <p>{time}</p>
+     </div>
+     <div class="list">
+       <h4>{title[1]}</h4>
+       <p>23.02.22 시작</p>
+       <p>{time}</p>
+     </div>
+     <div class="list">
+       <h4>{title[2]}</h4>
+       <p>23.02.22 시작</p>
+       <p>{time}</p>
+     </div>
     </div>
   );
 }
